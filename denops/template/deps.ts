82be48009caec9991a533ccbc80f3cd2abe873c2
skanehira <sho19921005@gmail.com>
2022-05-15 09:42:32 +0900

    Initial commit

---
+++ denops/template/deps.ts
@@ -0,0 +1,4 @@
+export * from "https://deno.land/x/denops_std@v2.2.0/mod.ts";
+export * from "https://deno.land/x/denops_std@v2.2.0/variable/mod.ts";
+export * from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
+export * from "https://deno.land/std@0.117.0/testing/asserts.ts";
