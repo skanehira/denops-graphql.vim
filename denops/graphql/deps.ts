b1e42eb0dea9fdd35e78220aff218b08557f1619
skanehira <sho19921005@gmail.com>
2023-01-06 23:10:11 +0900

    chore(test): replace denops_std/test to denops_test

--- denops/graphql/deps.ts
+++ denops/graphql/deps.ts
@@ -1,8 +1,6 @@
-export * from "https://deno.land/x/denops_std@v3.3.1/mod.ts";
-export * from "https://deno.land/x/denops_std@v3.3.1/variable/mod.ts";
-export * from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
-export * from "https://deno.land/std@0.139.0/testing/asserts.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.3.1/autocmd/mod.ts";
-export * from "https://deno.land/x/denops_std@v3.3.1/test/mod.ts";
-export * as fs from "https://deno.land/std@0.139.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.138.0/path/mod.ts";
+export * from "https://deno.land/x/denops_std@v3.12.1/mod.ts";
+export * from "https://deno.land/x/denops_std@v3.12.1/variable/mod.ts";
+export * from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.12.1/autocmd/mod.ts";
+export * as fs from "https://deno.land/std@0.171.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.171.0/path/mod.ts";
