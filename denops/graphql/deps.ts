41e5b0ef47ed594e759d36a2c3a3cf556b43b4a8
skanehira <sho19921005@gmail.com>
2023-02-11 13:22:48 +0900

    deps: update

--- denops/graphql/deps.ts
+++ denops/graphql/deps.ts
@@ -1,6 +1,6 @@
-export * from "https://deno.land/x/denops_std@v3.12.1/mod.ts";
-export * from "https://deno.land/x/denops_std@v3.12.1/variable/mod.ts";
+export * from "https://deno.land/x/denops_std@v4.0.0/mod.ts";
+export * from "https://deno.land/x/denops_std@v4.0.0/variable/mod.ts";
 export * from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.12.1/autocmd/mod.ts";
-export * as fs from "https://deno.land/std@0.171.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.171.0/path/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v4.0.0/autocmd/mod.ts";
+export * as fs from "https://deno.land/std@0.177.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.177.0/path/mod.ts";
